9201ed71f7168a17da1584162b36e717cb0c5d28
ann-fox <fx8565@mail.ru>
2023-12-28 13:12:38 +0500

    feat: add card component 86bwve8bv

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <h4 class="title">{{ title }}</h4>
+    <p class="secondary-text">{{ subText}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: String,
+    subText: String,
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  background-color: #fff;
+  border-radius: .2rem;
+  box-shadow: rgb(150, 150, 150) 0px 1px 3px;
+  width: 350px;
+  padding: .8rem;
+}
+</style>
