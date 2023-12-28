9b852d63eed7c62a5a7b7b6395a073775cb07802
ann-fox <fx8565@mail.ru>
2023-12-28 13:21:18 +0500

    feat: add card storybook 86bwve8bv

---
+++ src/components/Card.stories.js
@@ -0,0 +1,26 @@
+import Card from "./Card.vue";
+
+//👇 This default export determines where your story goes in the story list
+export default {
+    component: Card,
+};
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const FirstStory = {
+    render: (args) => ({
+        components: { Card },
+        setup() {
+            return { args };
+        },
+        template: '<Card v-bind="args" />',
+    }),
+    args: {
+        //👇 The args you need here will depend on your component
+        title: 'Primary',
+        subText: 'Example sub text'
+    },
+};
