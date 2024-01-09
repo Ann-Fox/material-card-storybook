2de4b1456dbc99d18aa19c3d59a620ab916efa70
ann-fox <fx8565@mail.ru>
2024-01-09 15:02:46 +0500

    feat: add icon in MyButton 86bwve8bv

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,9 +1,25 @@
 <template>
-  <button>test</button>
+  <button>
+    <img v-if="icon" :src="require(`icons/${icon}.svg`)" alt="icon" >
+    {{label}}
+  </button>
 </template>
 
 <script>
 export default {
-
+  props: {
+    label: {
+      type: String,
+      require: true,
+    },
+    filled: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+      default: '',
+    }
+  }
 }
 </script>
