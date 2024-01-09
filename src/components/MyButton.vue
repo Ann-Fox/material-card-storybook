1194483474c53af32faee2410c67ac3d65c6684a
ann-fox <fx8565@mail.ru>
2024-01-09 16:10:22 +0500

    feat: switching the button color in MyButton 86bwve8bv

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,11 +1,13 @@
 <template>
-  <button>
+  <button :class="classes" :style="backgroundColor">
     <img v-if="icon" :src="require(`icons/${icon}.svg`)" alt="icon" >
     {{label}}
   </button>
 </template>
 
 <script>
+import {computed, reactive} from "vue";
+
 export default {
   props: {
     label: {
@@ -19,7 +21,41 @@ export default {
     icon: {
       type: String,
       default: '',
+    },
+    backgroundColor: {
+      type: String
+    }
+  },
+  setup(props) {
+    props = reactive(props)
+    return {
+      classes: computed(()=> ({
+        'btn btn-primary': props.filled,
+        'btn btn-primary-primary': !props.filled,
+      })),
+      style: computed(()=>({
+        backgroundColor: props.backgroundColor
+      })),
     }
   }
 }
 </script>
+
+<style scoped>
+.btn {
+  width: 100px;
+  border: none;
+  font-size: 20px;
+}
+
+.btn-primary {
+  background-color: #66bf3c;
+  color: #a10a61;
+}
+
+.btn-primary-primary {
+  background-color: #1ea7fd;
+  color: #0e0d01;
+
+}
+</style>
