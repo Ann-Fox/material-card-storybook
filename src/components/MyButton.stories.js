8bb584cfb1a7d865f5935ee8f0dd1b1a73148ccb
ann-fox <fx8565@mail.ru>
2024-01-09 15:03:46 +0500

    feat: add FirstStory args in MyButton 86bwve8bv

--- src/components/MyButton.stories.js
+++ src/components/MyButton.stories.js
@@ -1,24 +1,21 @@
 import MyButton from './MyButton.vue';
 
-//👇 This default export determines where your story goes in the story list
 export default {
     component: MyButton,
 };
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/api/csf
- * to learn how to use render functions.
- */
-export const FirstStory = {
-    render: (args) => ({
+const Template = (args) => ({
         components: { MyButton },
         setup() {
             return { args };
         },
         template: '<MyButton v-bind="args" />',
-    }),
-    args: {
-        //👇 The args you need here will depend on your component
-    },
+
+});
+
+export const FirstStory = Template.bind({});
+
+FirstStory.args = {
+   label: "Save",
+    filled: true,
 };
