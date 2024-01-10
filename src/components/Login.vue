32c0357d64e23bfe68cb7670ee8050ab6e6141ec
ann-fox <fx8565@mail.ru>
2024-01-10 09:23:29 +0500

    feat: create template Login component 86bww1n6n

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="login-wrapper">
+    <div class="title">
+      <h3>Login</h3>
+    </div>
+    <div class="login-input">
+      <label for="email">email</label>
+      <input id="email" type="email">
+    </div>
+    <div class="login-input">
+      <label for="password">password</label>
+      <input id="password" type="password">
+    </div>
+    <button role="button" class="submit">
+      Submit
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
