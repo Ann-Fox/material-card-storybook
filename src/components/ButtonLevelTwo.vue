536183d75138d7bbc0a3f693bef9c8848db0a17e
ann-fox <fx8565@mail.ru>
2024-01-11 19:49:26 +0500

    feat: add style

--- src/components/ButtonLevelTwo.vue
+++ src/components/ButtonLevelTwo.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-<button>Кнопка</button>
+<button>Click me!</button>
 </template>
 
 <style scoped>
-
+button {
+  color: rgb(255, 255, 255);
+  font-size: 20px;
+  background-color: #2ee793;
+  border-radius: 20px;
+  padding: 20px;
+  border: 2px solid rgb(255, 255, 255);
+}
 </style>
